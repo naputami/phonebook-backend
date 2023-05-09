a314116d3990e3722f2738e7c8445409450a8dfb
Nadia Puji Utami <nadiapujiutami@gmail.com>
2023-05-09 19:51:48 +0700

    add build front-end

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const app = express()
 
 app.use(express.json())
 app.use(cors())
+app.use(express.static('build'))
 
 // morgan.token('req-body', (req) => JSON.stringify(req.body));
 // app.use(morgan(':method :url :status :res[content-length] - :response-time ms :req-body'));
