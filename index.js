75bb89b1e7393cf3ddb379d634dd0fb50404b29e
Nadia Puji Utami <nadiapujiutami@gmail.com>
2023-05-10 20:40:20 +0700

    add solution until excersise 3.22

--- index.js
+++ index.js
@@ -21,22 +21,22 @@ const errorHandler = (error, request, response, next) => {
 
   if (error.name === 'CastError') {
     return response.status(400).send({ error: 'malformatted id' })
-  } 
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).send({ error: error.message })
+  }
 
   next(error)
 }
-
 
 app.use(cors())
 app.use(express.json())
 app.use(requestLogger)
 app.use(express.static('build'))
 
-
 app.get('/api/persons', (request, response) => {
-    Person.find({}).then(persons => {
-      response.json(persons)
-    })
+  Person.find({}).then(persons => {
+    response.json(persons)
+  })
 })
 
 app.get('/api/info', (request, response) => {
@@ -46,59 +46,54 @@ app.get('/api/info', (request, response) => {
     const options = { weekday: 'short', day: 'numeric', month: 'short', year: 'numeric', hour: 'numeric', minute: 'numeric', second: 'numeric', timeZoneName: 'short' }
     response.send(`<p>Phonebook has info for ${count} people</p><br><p>${now.toLocaleString('en-US', options)}</p>`)
   })
-    
 })
 
 app.get('/api/persons/:id', (request, response, next) => {
-    Person.findById(request.params.id)
-      .then(person => {
-        if(person) {
-          response.json(person)
-        } else {
-          response.status(404).send({error: 'id is not found' })
-        }
-      })
-      .catch(error => next(error))
-    
+  Person.findById(request.params.id)
+    .then(person => {
+      if (person) {
+        response.json(person)
+      } else {
+        response.status(404).send({ error: 'id is not found' })
+      }
+    })
+    .catch(error => next(error))
 })
 
 app.delete('/api/persons/:id', (request, response, next) => {
-
-    Person.findByIdAndDelete(request.params.id)
-      .then(result =>{
-        if(result){
-          response.status(204).end()
-        } else {
-          response.status(404).send({error: 'id is not found' })
-        }
-        
-      })
-      .catch(error => next(error))
-   
+  Person.findByIdAndDelete(request.params.id)
+    .then(result => {
+      if (result) {
+        response.status(204).end()
+      } else {
+        response.status(404).send({ error: 'id is not found' })
+      }
+    })
+    .catch(error => next(error))
 })
 
-    
-app.post('/api/persons', (request, response) => {
-    const body = request.body
-  
-    if (!body.number || !body.name) {
-      return response.status(400).json({ 
-        error: 'name or number missing' 
-      })
-    }
-  
-    const person = new Person({
-      name: body.name,
-      number: body.number,
-    })
+app.post('/api/persons', (request, response, next) => {
+  const body = request.body
 
-    person.save().then(savedPerson=> {
+  if (!body.number || !body.name) {
+    return response.status(400).json({
+      error: 'name or number missing'
+    })
+  }
+
+  const person = new Person({
+    name: body.name,
+    number: body.number
+  })
+
+  person.save()
+    .then(savedPerson => {
       response.json(savedPerson)
     })
-     
+    .catch(error => next(error))
 })
 
-app.put('/api/persons/:id', (request, response) => {
+app.put('/api/persons/:id', (request, response, next) => {
   const body = request.body
 
   const person = {
@@ -106,21 +101,17 @@ app.put('/api/persons/:id', (request, response) => {
     number: body.number
   }
 
-  Person.findByIdAndUpdate(request.params.id, person, {new: true})
+  Person.findByIdAndUpdate(request.params.id, person, { new: true })
     .then(updatedNote => {
       response.json(updatedNote)
     })
     .catch(error => next(error))
 })
 
-
-
-
 app.use(unknownEndpoint)
 app.use(errorHandler)
-  
+
 const PORT = process.env.PORT
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
+  console.log(`Server running on port ${PORT}`)
 })
-
--- models/Person.js
+++ models/Person.js
@@ -1,25 +1,39 @@
 const mongoose = require('mongoose')
 
-mongoose.set('strictQuery',false)
+mongoose.set('strictQuery', false)
 
 const url = process.env.MONGODB_URI
- 
+
 console.log('connecting to', url)
 
 mongoose.connect(url)
-    .then(result => {
-        console.log('connected to MongoDB')
-    })
-    .catch(error => {
-        console.log('Error connecting to MongoDB', error.message )
-    })
+  .then(result => {
+    console.log('connected to MongoDB')
+  })
+  .catch(error => {
+    console.log('Error connecting to MongoDB', error.message)
+  })
 
 const personSchema = new mongoose.Schema({
-  name: String,
-  number: String,
+  name: {
+    type: String,
+    minLength: 3,
+    required: true
+  },
+  number: {
+    type: String,
+    minLength: 8,
+    required: true,
+    validate: {
+      validator: function (v) {
+        return /^\d{2,3}-\d+$/.test(v)
+      },
+      message: props => `${props.value} is not a valid number`
+    }
+  }
 })
 
-personSchema.set('toJSON',  {
+personSchema.set('toJSON', {
   transform: (document, returnedObject) => {
     returnedObject.id = returnedObject._id.toString()
     delete returnedObject._id
